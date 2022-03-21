27a2f8ebdbac9e65dad22f1bec8505af2cc778ee
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2022-03-22 01:23:53 +0600

    updated

--- student_result/index.php
+++ student_result/index.php
@@ -35,7 +35,7 @@
 <div class="container" >
     <div class="col-lg-4">
   <h2>Add a student</h2>
-  <form action="" name="student-form" method="post">
+  <form action="" name="student-form" method="post" enctype="multipart/form-data">
     <div class="form-group">
       <label for="sid">Student ID:</label>
       <input type="text" class="form-control" id="sid" placeholder="Enter id" name="sid">
@@ -53,10 +53,14 @@
       <input type="text" class="form-control" id="address" placeholder="Enter address" name="address">
     </div>
     <div class="form-group">
+      <label for="image">Image:</label>
+      <input type="file" class="form-control" id="file" name="file">
+    </div>
+    <!--<div class="form-group">
       <label for="result">Result:</label>
       <input type="number" step="any" class="form-control" id="result" placeholder="Enter result" name="result">
     </div>
-    <!-- <div class="form-group">
+     <div class="form-group">
       <label for="datetime">Date Created:</label>
       <input type="datetime-local" class="form-control" id="datetime" placeholder="Enter date-time" name="datetime">
     </div> -->
@@ -65,6 +69,8 @@
   </form>
   </div>
 </div>
+
+
 
 <div class="container" style="margin:20px"></div>
 <div class="container">
@@ -84,15 +90,31 @@
     
     <tbody>
       <?php 
+        function round_up ( $value, $precision ) { 
+          $pow = pow ( 10, $precision ); 
+          return ( ceil ( $pow * $value ) + ceil ( $pow * $value - ceil ( $pow * $value ) ) ) / $pow; 
+        }
+        function getResult($connection, $sid){
+          $score = 0.0;
+          $count = 0;
+          $results = mysqli_query($connection, "select * from result where SID=$sid") or die(mysqli_error($connection));
+          while($result=mysqli_fetch_array($results)){
+            $score += $result["Result"];
+            $count += 1;
+          }
+          if ($count<=0) return 0.0;
+          return round_up($score/$count,2);  
+        }
+
         $rows = mysqli_query($connection, "select * from student");
         $sid = "";
         while($row=mysqli_fetch_array($rows)){
             //$sid = $row["SID"];
-            echo "<tr>";
+            echo '<tr onclick="details(this)">';
                 echo "<td>"; echo $row["SID"]; echo "</td>";
                 echo "<td>"; echo $row["Name"]; echo "</td>";
                 echo "<td>"; echo $row["Address"]; echo "</td>";
-                echo "<td>"; echo $row["Result"]; echo "</td>";
+                echo "<td>"; echo getResult($connection, $row["SID"]); echo "</td>";
                 echo '<td style="text-align:center">'; ?> <a href="edit.php?sid=<?php echo $row["SID"]; ?>">
                 <button type="button" class="btn btn-success">Edit</button></a> <?php echo "</td>";
 
@@ -122,12 +144,19 @@
 </div>
 
 <?php
-    //echo DateTime();
+    
     if(isset($_POST["save"])){
-        //echo "Sadii";
+
+        $unique_key = md5(time());
+        $file_name = $_FILES["file"]["name"];
+        $destination = './images/'.$unique_key.$file_name;
+        $destination_for_table = 'images/'.$unique_key.$file_name;
+        move_uploaded_file($_FILES["file"]["tmp_name"], $destination);
+        $res = getResult($connection, $_POST["sid"]);
+
         $date_time = date('Y-m-d H:i:s');
         mysqli_query($connection, "insert into student values('$_POST[sid]', '$_POST[name]', '$_POST[age]', '$_POST[address]',
-        '$_POST[result]', '$date_time')") or die(mysqli_error($connection)); //$_POST[datetime]
+        '$res', '$date_time', '$destination_for_table')") or die(mysqli_error($connection)); //$_POST[datetime]
         ?>
         <script type="text/javascript">
             window.location.href = window.location.href;
@@ -149,6 +178,14 @@
     }
     
 ?>
+
+<script>
+  function details(x){
+    //alert(x.cells[0].innerHTML);
+    sid = x.cells[0].innerHTML;
+    window.location.href = 'details.php?sid='+sid;
+  }
+</script>
 
 <!-- <div id="delete"></div>
 <script>
