41f1513e5e0ab9225d51040d1f2606279a4e12c6
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2022-03-31 16:22:30 +0600

    updated code
    form validation, db connection

--- student_result/index.php
+++ student_result/index.php
@@ -1,6 +1,64 @@
 <?php
     include "db_connection.php";
+    include 'getResult.php';
     date_default_timezone_set('Asia/Dhaka');
+
+    $nameErr = $sidErr = $ageErr = $addressErr = $ImageErr = "";
+    $name = $sid = $age = $address = $image = "";
+
+    function input_data($data) {  
+      $data = trim($data);  
+      $data = stripslashes($data);  
+      $data = htmlspecialchars($data);  
+      return $data;  
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {  
+      //name validation
+      if (empty($_POST["name"])) {  
+        $nameErr = "Name is required";  
+      } else {  
+          $name = input_data($_POST["name"]);  
+           // check if name only contains letters and whitespace  
+           if (!preg_match("/^[a-zA-Z ]*$/",$name)) {  
+               $nameErr = "Only alphabets and white space are allowed";  
+           }  
+      }
+      
+      //id validation
+      if (empty($_POST["sid"])) {  
+        $sidErr = "Student Id is required";  
+      } else {  
+        $sid = input_data($_POST["sid"]);  
+        // check if mobile no is well-formed  
+        if (!preg_match ("/^[0-9]*$/", $sid) ) {  
+        $sidErr = "Only numeric value is allowed.";  
+        }  
+        //check mobile no length should not be less and greator than 10  
+        if (strlen ($sid) != 4) {  
+        $sidErr = "Student Id must contain 4 digits.";  
+        }  
+      }
+      
+      if (empty($_POST["age"])) {  
+        $ageErr = "Age is required";  
+      } else {  
+        $age = input_data($_POST["age"]);  
+        if ($age<"18" || $age>="30") {  
+          $ageErr = "Age must be between 18 and 30.";  
+        }
+      }
+
+      if (empty($_POST["address"])) {  
+        $addressErr = "Address is required";  
+      } else {  
+        $address = input_data($_POST["address"]);  
+      }
+    
+    }
+
+
+    
 
 ?>
 <!DOCTYPE html>
@@ -28,6 +86,12 @@
         tr:nth-child(even) {
             background-color: #dddddd;
         }
+        button{
+          width: 70px;
+        }
+        .error{
+          color: #FF0001;
+        } 
     </style>
 </head>
 <body>
@@ -35,22 +99,26 @@
 <div class="container" >
     <div class="col-lg-4">
   <h2>Add a student</h2>
-  <form action="" name="student-form" method="post" enctype="multipart/form-data">
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" name="student-form" method="post" enctype="multipart/form-data">
     <div class="form-group">
       <label for="sid">Student ID:</label>
       <input type="text" class="form-control" id="sid" placeholder="Enter id" name="sid">
+      <span class="error">*<?php echo $sidErr; ?> </span> 
     </div>
     <div class="form-group">
       <label for="name">Name:</label>
       <input type="text" class="form-control" id="name" placeholder="Enter name" name="name">
+      <span class="error">*<?php echo $nameErr; ?> </span> 
     </div>
     <div class="form-group">
       <label for="age">Age:</label>
       <input type="number" class="form-control" id="age" placeholder="Enter age" name="age">
+      <span class="error">*<?php echo $ageErr; ?> </span> 
     </div>
     <div class="form-group">
       <label for="address">Address:</label>
       <input type="text" class="form-control" id="address" placeholder="Enter address" name="address">
+      <span class="error">* <?php echo $addressErr; ?> </span> 
     </div>
     <div class="form-group">
       <label for="image">Image:</label>
@@ -90,21 +158,7 @@
     
     <tbody>
       <?php 
-        function round_up ( $value, $precision ) { 
-          $pow = pow ( 10, $precision ); 
-          return ( ceil ( $pow * $value ) + ceil ( $pow * $value - ceil ( $pow * $value ) ) ) / $pow; 
-        }
-        function getResult($connection, $sid){
-          $score = 0.0;
-          $count = 0;
-          $results = mysqli_query($connection, "select * from result where SID=$sid") or die(mysqli_error($connection));
-          while($result=mysqli_fetch_array($results)){
-            $score += $result["Result"];
-            $count += 1;
-          }
-          if ($count<=0) return 0.0;
-          return round_up($score/$count,2);  
-        }
+        
 
         $rows = mysqli_query($connection, "select * from student");
         $sid = "";
@@ -114,7 +168,7 @@
                 echo "<td>"; echo $row["SID"]; echo "</td>";
                 echo "<td>"; echo $row["Name"]; echo "</td>";
                 echo "<td>"; echo $row["Address"]; echo "</td>";
-                echo "<td>"; echo getResult($connection, $row["SID"]); echo "</td>";
+                echo "<td>"; echo getOverallResult($connection, $row["SID"]); echo "</td>";
                 echo '<td style="text-align:center">'; ?> <a href="edit.php?sid=<?php echo $row["SID"]; ?>">
                 <button type="button" class="btn btn-success">Edit</button></a> <?php echo "</td>";
 
@@ -147,22 +201,26 @@
     
     if(isset($_POST["save"])){
 
+      if($sidErr=="" && $nameErr=="" && $ageErr=="" && $addressErr==""){
+
         $unique_key = md5(time());
         $file_name = $_FILES["file"]["name"];
         $destination = './images/'.$unique_key.$file_name;
         $destination_for_table = 'images/'.$unique_key.$file_name;
         move_uploaded_file($_FILES["file"]["tmp_name"], $destination);
-        $res = getResult($connection, $_POST["sid"]);
+        $res = getOverallResult($connection, $_POST["sid"]);
 
         $date_time = date('Y-m-d H:i:s');
-        mysqli_query($connection, "insert into student values('$_POST[sid]', '$_POST[name]', '$_POST[age]', '$_POST[address]',
+        mysqli_query($connection, "insert into student values('$sid', '$name', '$age', '$address',
         '$res', '$date_time', '$destination_for_table')") or die(mysqli_error($connection)); //$_POST[datetime]
         ?>
         <script type="text/javascript">
             window.location.href = window.location.href;
         </script>
         <?php
+      }
     }
+
     if (isset($_POST['Delete'])) {
         // If you receive the Delete post data, delete it from your table
         // $delete = 'DELETE FROM student WHERE SID = ?';
@@ -170,6 +228,8 @@
         // $stmt->bind_param("i", $_POST['Delete']);
         // $stmt->execute();
         mysqli_query($connection, "delete from student where SID='$_POST[Delete]'") or die(mysqli_error($connection));
+        mysqli_query($connection, "delete from result where SID='$_POST[Delete]'") or die(mysqli_error($connection));
+        mysqli_query($connection, "delete from course where SID='$_POST[Delete]'") or die(mysqli_error($connection));
         ?>
         <script type="text/javascript">
             window.location.href = window.location.href;
